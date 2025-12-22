3788eaea9689bec1fef1d833516b3352fc9486ac
ReAct22 <41190326+ReAct22@users.noreply.github.com>
2025-12-22 10:40:58 +0700

    Add route for storing customer agent response
    Introduces a new POST route 'store/respon' mapped to the 'store' method of CustomerAgentContoller. Removes the default authenticated user route.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\api\CustomerAgentContoller;
+use App\Models\CustomerAgent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('store/respon', [CustomerAgentContoller::class, 'store']);
